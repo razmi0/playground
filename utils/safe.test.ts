791a4ccacdeef36d7211740559c9dc07c3b1ba38
razmiO <thomas.cuesta31@gmail.com>
2025-01-08 16:14:11 +0100

    test : add safe nominal test

--- utils/safe.test.ts
+++ utils/safe.test.ts
@@ -1,17 +1,17 @@
 import { assert, assertThrows } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
 import { safe } from "./safe.ts";
 
-describe("safe function", () => {
-    it("should catch errors", () => {
-        assertThrows(() => {
-            safe(() => {
-                throw new Error("test");
-            });
+// Test: Should catch errors
+Deno.test("[safe] : should catch errors", () => {
+    assertThrows(() => {
+        safe(() => {
+            throw new Error("test");
         });
     });
+});
 
-    it("should return the result of the function", () => {
-        assert(safe(() => "test") === "test");
-    });
+// Test: Should return the result of the function
+Deno.test("[safe] : should return the result of the function", () => {
+    const result = safe(() => "test");
+    assert(result === "test");
 });

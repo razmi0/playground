f7ebd3c771e34a5c4531578fbd824f08efc2402e
razmiO <thomas.cuesta31@gmail.com>
2025-01-08 16:13:03 +0100

    switch to Deno.test

--- fetch/main.test.ts
+++ fetch/main.test.ts
@@ -1,91 +1,67 @@
-// deno run test --allow-net fetch/main_test.ts
 import { assertEquals, assertRejects } from "@std/assert";
-import { describe, it } from "@std/testing/bdd";
 import { mockFetch } from "../test_helpers/utils.ts";
 import { fetchWithCallbacks } from "./main.ts";
 
 type APIFetchData = {
     success: {
         message: "Success";
-    };
+    }[];
     error: {
         message: "Not Found";
-    };
+    }[];
 };
 
-describe("fetchWithCallbacks function", () => {
-    /**
-     *
-     */
-
-    it("[onSuccess] : should handle successful fetch", async () => {
-        mockFetch({
-            expected: "success",
-            returnData: {
-                message: "Success",
-            },
-        });
-        const result = await fetchWithCallbacks<APIFetchData["success"]>("", {
-            onSuccess: (_response, json) => {
-                return json;
-            },
-        });
-
-        assertEquals(result.response.status, 200);
-        assertEquals(result.data, { message: "Success" });
+// Test: Handle successful fetch
+Deno.test("[onSuccess] : should handle successful fetch", async () => {
+    mockFetch({
+        expected: "success",
+        returnData: {
+            message: "Success",
+        },
     });
 
-    /**
-     *
-     */
+    const result = await fetchWithCallbacks<APIFetchData["success"]>("", {
+        onSuccess: (_response, json) => [json, "additional data"],
+    });
 
-    it("[onError] : should handle fetch error 404", async () => {
-        mockFetch({
-            expected: "error",
-            returnData: {
-                message: "Not Found",
+    assertEquals(result.response.status, 200);
+    assertEquals(result.data, [{ message: "Success" }, "additional data"]);
+});
+
+// Test: Handle fetch error 404
+Deno.test("[onError] : should handle fetch error 404", async () => {
+    mockFetch({
+        expected: "error",
+        returnData: {
+            message: "Not Found",
+        },
+    });
+
+    const result = await fetchWithCallbacks<never, APIFetchData["error"]>("", {
+        onError: (_response, json) => [json, "additional error data"],
+    });
+
+    assertEquals(result.data, [{ message: "Not Found" }, "additional error data"]);
+    assertEquals(result.response.status, 404);
+});
+
+// Test: Handle promise rejection in before callback
+Deno.test("[before] : should handle promise rejection in before callback", () => {
+    assertRejects(() => {
+        return fetchWithCallbacks<never, never>("", {
+            before: () => {
+                throw new Error("Error");
             },
         });
-        // Mock fetch to return a 404 error
+    });
+});
 
-        const result = await fetchWithCallbacks<never, APIFetchData["error"]>("", {
-            onError: (_response, json) => {
-                return json;
-            },
-        });
-
-        assertEquals(result.data, { message: "Not Found" });
-        assertEquals(result.response.status, 404);
+// Test: Return the before callback's return data in after callback
+Deno.test("[after] : should return the before callback's return data", async () => {
+    const result = await fetchWithCallbacks<never, never, "Hi from before">("", {
+        before: () => "Hi from before",
+        after: (beforeReturn) => beforeReturn,
     });
 
-    /**
-     *
-     */
-
-    it("[before] : should handle throw error in before cb", () => {
-        assertRejects(() => {
-            return fetchWithCallbacks<never, never>("", {
-                before: () => {
-                    throw new Error("Error");
-                },
-            });
-        });
-    });
-
-    /**
-     *
-     */
-    it("[after] : should return the before cb return data", async () => {
-        const result = await fetchWithCallbacks<{
-            userId: number;
-            id: number;
-            title: string;
-            completed: boolean;
-        }>("", {
-            before: () => "Hi from before",
-            after: (beforeReturn) => beforeReturn,
-        });
-
-        assertEquals(result.afterData, "Hi from before");
-    });
+    assertEquals(result.afterData, "Hi from before");
 });

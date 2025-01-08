3dbcd6c8a49afc1b23e4bfdd20edd47ca03f2813
razmiO <thomas.cuesta31@gmail.com>
2025-01-08 16:56:09 +0100

    feat(lib) : add mod.ts barrel file

---
+++ helpers/mod.ts
@@ -0,0 +1,5 @@
+export { colorfull, colors, type Colors } from "./src/colorfull/main.ts";
+export { fetchWithCallbacks } from "./src/fetch/main.ts";
+export { safe } from "./src/safe/main.ts";
+export { Spinner, type SpinnerConfig } from "./src/spinner/main.ts";
+export { wait } from "./src/wait/main.ts";
